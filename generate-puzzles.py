578ca44075742e7ef950c04f33b83c6644bb97c5
superandybean <andrewlamm04@gmail.com>
2023-07-28 01:41:08 -0400

    add naive sol

--- generate-puzzles.py
+++ generate-puzzles.py
@@ -1,15 +1,37 @@
 import csv
+import random
 
 player_data = {}
 date_updated = None
 
 team_players = {}
+teams = []
+preferable_teams = []
 
-grid = [[((0, 3), (1, 3), (2, 3))], [(0, 4), (1, 4), (2, 4)], [(0, 5), (1, 5), (2, 5)]]
-STATS = [('country', None), ('age', [20, 25, 30]), ('rating2', [1.1, 1.2]), ('rating1', [1.1, 1.2]), ('maps', [1000, 2000, 3000]), ('rounds', [20000, 30000, 40000]), ('kills', [10000, 20000, 30000]), ('deaths', [10000, 20000, 30000]), ('ratingTop20')]
+PUZZLES_GRID = [(0, 3), (1, 3), (2, 3), (0, 4), (1, 4), (2, 4), (0, 5), (1, 5), (2, 5)]
+STATS = [
+  ('country', None),
+  ('age', [20, 25, 30]),
+  ('rating2', [1.1, 1.2]),
+  ('rating1', [1.1, 1.2]),
+  ('maps', [1000, 2000, 3000]),
+  ('rounds', [20000, 30000, 40000]),
+  ('kills', [10000, 20000, 30000]),
+  ('deaths', [10000, 20000, 30000]),
+  ('ratingTop20', [1.1, 1.2]),
+  ('ratingYear', [[2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022], [1.1, 1.2]]),
+  ('clutchesTotal', [250, 500, 600]),
+  ('majorsWon', [1, 2]),
+  ('majorsPlayed', [1, 4, 8]),
+  ('LANsWon', [1, 5, 10]),
+  ('MVPs', [1, 3, 5, 10]),
+  ('top20s', [1, 3, 5, 10]),
+  ('top10s', [1, 3, 5]),
+  ('topPlacement', [1, 5, 10, 20])
+]
 
 def read_data():
-  with open("playerData.csv", 'r') as file:
+  with open("playerData.csv", 'r', encoding="utf8") as file:
     csvreader = csv.reader(file)
     top_row = next(csvreader)
     parse_type = ['', 'int', '', '', 'int', 'float', 'float', 'int', 'int', 'int', 'int', 'int', 'float', 'float', 'float', 'float', 'dictionary', 'int', 'set', 'int', 'int', 'int', 'int', 'int', 'int', 'int', 'int']
@@ -43,9 +65,85 @@ def preprocess_data():
     for team in player_data[player_id]['teams']:
       if team not in team_players:
         team_players[team] = set()
+        teams.append(team)
       team_players[team].add(player_id)
 
+  # find the tuple of teams that work
+  tuple_teams = []
+  for i in range(len(teams)):
+    for j in range(i+1, len(teams)):
+      if len(team_players[teams[i]].intersection(team_players[teams[j]])) > 0:
+        tuple_teams.append((teams[i], teams[j]))
+
+  pref_teams = set()
+  for team1, team2 in tuple_teams:
+    pref_teams.add(team1)
+    pref_teams.add(team2)
+
+  for team in pref_teams:
+    preferable_teams.append(team)
+
+def generate_player_set(clue1, clue2):
+  clue1_possible = set()
+  clue2_possible = set()
+
+  if clue1[0] == 'team':
+    for player in team_players[clue1[1]]:
+      clue1_possible.add(player)
+  else:
+    # TODO: skip for now
+    pass
+
+  if clue2[0] == 'team':
+    for player in team_players[clue2[1]]:
+      clue2_possible.add(player)
+  else:
+    # TODO: skip for now
+    pass
+
+  return clue1_possible.intersection(clue2_possible)
+
+def solve_puzzle(puzzle, curr_board, curr_spot, player_set):
+  if curr_spot >= 9:
+    return curr_board
+
+  clue_pos = PUZZLES_GRID[curr_spot]
+  clue1 = puzzle[clue_pos[0]]
+  clue2 = puzzle[clue_pos[1]]
+
+  possible_players = generate_player_set(clue1, clue2)
+
+  for player in possible_players:
+    if player in player_set:
+      continue
+    player_set_dup = player_set.copy()
+    curr_board_dup = curr_board.copy()
+    player_set_dup.add(player)
+    curr_board_dup[curr_spot] = player
+
+    ans = solve_puzzle(puzzle, curr_board_dup, curr_spot+1, player_set_dup)
+    if ans is not None:
+      return ans
+
+  return None
+
 def generate_puzzle():
-  pass
+  while True:
+    puzzle = []
+    clues = set()
+    for i in range(6):
+      curr_clue = random.choice(preferable_teams)
+      while curr_clue in clues:
+        curr_clue = random.choice(preferable_teams)
+      clues.add(curr_clue)
+      puzzle.append(['team', curr_clue])
+
+    print('attempting to solve', puzzle)
+    ans = solve_puzzle(puzzle, [None, None, None, None, None, None, None, None, None], 0, set())
+    if ans is not None:
+      return puzzle, ans
 
 read_data()
+preprocess_data()
+
+print(generate_puzzle())
